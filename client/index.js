561ae2c53ee1c23280a5827c78bfd89969701156
MirekProjects <neji000hyuuga@seznam.cz>
2026-01-11 13:02:20 +0100

    added logic for managing active typing state

--- client/index.js
+++ client/index.js
@@ -6,9 +6,9 @@ function createClientId() {
   const randChars = (chars, length) =>
     Array.from({ length }, () => chars[Math.floor(Math.random() * chars.length)]).join("");
 
-  const part1 = randChars(letters, 3);          
-  const part2 = randChars(numbers, 3);         
-  const part3 = randChars(letters + numbers, 3); 
+  const part1 = randChars(letters, 3);
+  const part2 = randChars(numbers, 3);
+  const part3 = randChars(letters + numbers, 3);
 
   return `${part1}-${part2}-${part3}`;
 }
@@ -19,47 +19,76 @@ const clientId = createClientId()
 const socket = new WebSocket("ws://localhost:8080");
 // Connection opened
 socket.addEventListener("open", (event) => {
-  socket.send(JSON.stringify({"clientId": clientId, "connection": true}));
+  socket.send(JSON.stringify({ "clientId": clientId, "connection": true }));
 });
 
-document.getElementById("send").onclick = function(){
-    sendMessage()
+document.getElementById("send").onclick = function () {
+  sendMessage()
 };
 
 addEventListener("keydown", function (event) {
-    if (event.key === "Enter") {
-      event.preventDefault()
-      sendMessage()
-    }
+  if (event.key === "Enter") {
+    event.preventDefault()
+    sendMessage()
+  }
 })
 
+const chatboxMessageField = document.getElementById("chatbox-message-field");
+
 const sendMessage = () => {
-  var userMessage = document.getElementById("chatbox-message-field").value;
-  if(userMessage == "") {
+  var userMessage = chatboxMessageField.value;
+  typingActive = false;
+  if (userMessage == "") {
     return
-  } 
-  socket.send(JSON.stringify({"clientId": clientId, "userMessage": userMessage}))
+  }
+  console.log("xDDDD")
+  socket.send(JSON.stringify({ "clientId": clientId, "userMessage": userMessage }))
   displayMessage(clientId + " says: " + userMessage)
 }
 
 // Listen for messages
 socket.addEventListener("message", (event) => {
-  if(JSON.parse(event.data).connection == true) {
-    displayMessage(JSON.parse(event.data).clientId + " connected to the chatroom")
-  } else if (JSON.parse(event.data).clientId == clientId) {
+  const incomingMessage = JSON.parse(event.data)
+
+  if (incomingMessage.connection == true) {
+    displayMessage(incomingMessage.clientId + " connected to the chatroom")
+  } else if (incomingMessage.clientId == clientId && incomingMessage.userMessage) {
     // matching Ids
-    document.getElementById("chatbox-message-field").value = ""
+    chatboxMessageField.value = ""
   } else {
     // not matching Ids
-    const incomingMessage = JSON.parse(event.data)
+    if (incomingMessage.typing || !incomingMessage.typing) {
+      console.log("User " + incomingMessage.clientId + "is typing...")
+      return
+    }
     displayMessage(incomingMessage.clientId + " says: " + incomingMessage.userMessage)
   }
 });
 
-
-function displayMessage(userMessage) {
+const displayMessage = (userMessage) => {
   const messageDiv = document.createElement("div");
-  messageDiv.textContent = userMessage;   
+  messageDiv.textContent = userMessage;
   messageDiv.classList.add("message");
   document.getElementById("chatbox-text").appendChild(messageDiv);
 }
+
+
+
+let typingTimeout;
+let typingActive = false;
+
+chatboxMessageField.addEventListener("input", () => {
+  if (!typingActive) {
+    typingActive = true;
+    socket.send(JSON.stringify({"clientId": clientId, "typing": true})
+  )}
+
+  clearTimeout(typingTimeout);
+
+  typingTimeout = setTimeout(() => {
+    typingActive = false;
+    socket.send(JSON.stringify({ "clientId": clientId, "typing": false})
+  );
+  }, 5000);
+});
+
